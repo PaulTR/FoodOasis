8ba785addc11c028ed443c2c17512edfca779ff7
Paul Ruiz <ptruiz@google.com>
2020-12-05 11:16:46 -0700

    updated functions

--- mobile/app/src/main/java/com/dukehack/foodoasis/MapsActivity.java
+++ mobile/app/src/main/java/com/dukehack/foodoasis/MapsActivity.java
@@ -1,15 +1,27 @@
 package com.dukehack.foodoasis;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.FirebaseFunctionsException;
+import com.google.firebase.functions.HttpsCallableResult;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -18,29 +30,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng denver = new LatLng(39.7392, -104.9903);
+        mMap.addMarker(new MarkerOptions().position(denver).title("Marker"));
+        CameraPosition position = new CameraPosition.Builder()
+                .zoom(13)
+                .target(denver)
+                .build();
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(position));
+        Map<String, Object> data = new HashMap<>();
+        data.put("lat", 39.7392);
+        data.put("lng", -104.9903);
+
+        FirebaseFunctions.getInstance().getHttpsCallable("getPointScore")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, String>() {
+                    @Override
+                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        Log.e("Test", "we got something!");
+                        if (!task.isSuccessful()) {
+                            Log.e("Test", "unsuccessful");
+                            Exception e = task.getException();
+                            Log.e("test", e.getMessage());
+
+                        } else {
+                            Log.e("Test", "data returned: " + task.getResult().getData().toString());
+                        }
+
+                        return null;
+                    }
+                });
     }
 }
