4b9bc366ab05233dbba3597125e10f2d98ec5b5f
Paul Ruiz <ptruiz@google.com>
2020-12-05 09:28:33 -0700

    connected android app to firebase, initialized map

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -3,7 +3,12 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => 
+{
+ var inputtext = event.data.child('text').val();
+ var d = new Date();
+ var reply = "{\"data\": {\"message\": " + inputtext +", \"timestamp\": \""+d.getMilliseconds()+"\"}}";
+ console.log(reply)
+ response.send(reply);
+});
+
