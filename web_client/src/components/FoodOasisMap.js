8034e628f857cfea0a90524acd05b3b80007ee16
DavidHanover <DavidHanover89@gmail.com>
2020-12-05 21:05:32 -0800

    clicking client attempts to send payload to endpoint.  debugging CORS problems now.

--- web_client/src/components/FoodOasisMap.js
+++ web_client/src/components/FoodOasisMap.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GoogleMapReact from "google-map-react";
+import axios from "axios";
 import "./style/foodOasisMap.css";
 
-const _onClick = ({ lat, lng }) =>
-  console.log("latitude of click: ", lat, "longitude of click: ", lng);
+export const FoodOasisMap = ({ location, zoomLevel, googleMapsAPIKey }) => {
+  let [lastCoordinates, setLastCoordinates] = useState({
+    mostRecentUserLAT: 0,
+    mostRecentUserLNG: 0,
+  });
+  //function handler for user clicking on the map, gets mapped to GoogleMapReact component's "onClick" event below
+  const _onClick = ({ lat, lng }) => {
+    setLastCoordinates({ mostRecentUserLAT: lat, mostRecentUserLNG: lng });
+  };
+  //hook runs when user updates last coordinates
+  useEffect(() => {
+    console.log(
+      "latitude of click: ",
+      lastCoordinates.mostRecentUserLAT,
+      "longitude of click: ",
+      lastCoordinates.mostRecentUserLNG
+    );
+    axios({
+      method: "post",
+      url:
+        "https://us-central1-food-oasis-f4d73.cloudfunctions.net/getPointScore",
+      headers: { "content-type": "application/json" },
+      data: {
+        lat: lastCoordinates.mostRecentUserLAT,
+        lng: lastCoordinates.mostRecentUserLNG,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [lastCoordinates]);
+  return (
+    <div className="map">
+      <h2 className="map-h2">Click on the map to see FoodOasis score!</h2>
 
-export const FoodOasisMap = ({ location, zoomLevel, googleMapsAPIKey }) => (
-  <div className="map">
-    <h2 className="map-h2">Click on the map to see FoodOasis score!</h2>
-
-    <div className="google-map">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: googleMapsAPIKey }}
-        defaultCenter={location}
-        defaultZoom={zoomLevel}
-        onClick={_onClick}
-      />
+      <div className="google-map">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: googleMapsAPIKey }}
+          defaultCenter={location}
+          defaultZoom={zoomLevel}
+          onClick={_onClick}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default FoodOasisMap;
